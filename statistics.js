c854ba9885bddad6f752d4d5ece2b1e46b8b555f
Eddin Amaya <badreddin.amaya@skola.botkyrka.se>
2020-11-24 13:22:31 +0100

    .\statistics.js/fisrtcommit

--- statistics.js
+++ statistics.js
@@ -1,12 +1,16 @@
 
-function max(arr) {
+function max(arr) { 
+    return Math.max(arr)
 
 }
+console.log(reduce(2, 1, 5, 8 ,4 ,5 ,9))
 
 function min(arr) {
+    return Math.min(arr)
 
 }
 
 function mean(arr) {
-    // Ledtråd: Börja med att räkna ut summan av alla element
+    reduce(arr)
+    return reduce(arr)    // Ledtråd: Börja med att räkna ut summan av alla element
 }
