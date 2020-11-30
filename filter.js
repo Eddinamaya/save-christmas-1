314d03bd81db22e610a527a641de2c86cf4dffc3
Eddin Amaya <badreddin.amaya@skola.botkyrka.se>
2020-11-30 15:04:09 +0100

    steg4

--- filter.js
+++ filter.js
@@ -1,8 +1,14 @@
 
 function filter(arr, func) {
-
+let newArray = []
+for(let i = 0; i < arr.length; i++){
+    if (func(arr[i]) === true) {
+        newArray.push(arr[i])
+        }
+        
 }
-
+return newArray
+}
 // TEST 1
 const numbers = [4, 10, 3, 12, 5, 16, 8, 13, 2, 9, 1, 11, 2]
 const filteredNumbers = filter(numbers, x => x < 10)
