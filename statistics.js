b735157cd3803967008c9c8bf64f53512ce6e9d8
Eddin Amaya <badreddin.amaya@skola.botkyrka.se>
2020-11-30 14:17:20 +0100

    steg2

--- statistics.js
+++ statistics.js
@@ -1,16 +1,32 @@
 
-function max(arr) { 
-    return Math.max(arr)
+function max(arr) {
 
+    let largest = arr[0]
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > largest){
+         largest = arr[i]
+        }
+    }
+    return largest
 }
-console.log(reduce(2, 1, 5, 8 ,4 ,5 ,9))
+console.log(mean([2, 1, 5, 8 ,4 ,5 ,9]))
 
 function min(arr) {
-    return Math.min(arr)
 
+    let smallest = arr[0]
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] < smallest){
+           smallest = arr[i] 
+        }
+    }
+    return smallest
 }
 
 function mean(arr) {
-    reduce(arr)
-    return reduce(arr)    // Ledtråd: Börja med att räkna ut summan av alla element
+    let result = 0
+    for (let i = 0; i < arr.length; i++) {
+        result = result + arr[i]
+    }
+    return result/arr.length
+        // Ledtråd: Börja med att räkna ut summan av alla element
 }
